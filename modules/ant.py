879dd2b4bde2429c1401ac5bac95bf66e7bd3db8
Joseph <mateuszpieczulis123@gmail.com>
2021-07-17 21:54:21 +0200

    started sensors on ants

--- modules/ant.py
+++ modules/ant.py
@@ -1,37 +1,90 @@
 import pygame
 import random
+import math
 
 from modules.colors import colors
 
-class Ant():
-    def __init__(self, surface, x, y):
+class Sensor():
+    def __init__(self, x, y, id_, antRadius):
         self.x = x
         self.y = y
 
-        self.radius = 7
-        self.radiusHalf = self.radius//2
+        self.scale = 4
+        self.antRadius = antRadius
+        self.radius = self.antRadius * self.scale
 
-        self.velX = 0
-        self.velY = 0
+        self.offset = self.setOffsetByID(id_)
 
-        self.searching = True
+    def sense(self):
+        pass
 
-    def draw(self, surface):
-        rect = pygame.draw.circle(surface, colors['grey'], (self.x, self.y), self.radius)
+    def testDraw(self, surface):
+        rect = pygame.draw.circle(surface, colors['white'], (self.x+self.offset[0], self.y+self.offset[1]), self.radius)
 
     def setPosition(self, x, y):
         self.x, self.y = x, y
 
+    def setDirestion(self):
+        pass
+
+    def setOffset(self, x, y):
+        self.offset = (x, y)
+
+    def setOffsetByID(self, id_):
+        distance = self.antRadius*self.scale
+
+        if id_ == 0:
+            return (-distance, -distance)
+        elif id_ == 1:
+            return (0, -distance*2)
+        elif id_ == 2:
+            return (distance, -distance)
+
+class Ant(Sensor):
+    def __init__(self, surface, x, y):
+        self.x = x
+        self.y = y
+
+        self.radius = 2
+
+        self.velX = 0.0
+        self.velY = 0.0
+
+        self.angle = 0
+        self.rotationSpeed = 15
+        self.speed = 1
+
+        self.targetPos = (0, 0)
+        self.lastDirection = self.angle
+
+        self.searching = True
+        self.sensors = [Sensor(self.x, self.y, 0, self.radius), Sensor(self.x, self.y, 1, self.radius), Sensor(self.x, self.y, 2, self.radius)]
+
+    def draw(self, surface):
+        rect = pygame.draw.circle(surface, colors['grey'], (self.x, self.y), self.radius)
+        for sensor in self.sensors:
+            sensor.testDraw(surface)
+
+    def setPosition(self, x, y):
+        self.x, self.y = x, y
+        for sensor in self.sensors:
+            sensor.setPosition(self.x, self.y)
+
     def move(self):
         if self.searching:
-            self.velX += random.randint(-1, 1)
-            self.velY += random.randint(-1, 1)
+            self.rotationSpeed = random.randint(0, 20)
+            self.speed = random.randint(0, 2)
+            angleNew = random.randint(0 , 360)
 
-            self.x += self.velX
-            self.y += self.velY
+            self.angle = abs(self.angle - angleNew - self.rotationSpeed)
+
+            self.velX = math.cos(self.angle) + self.speed
+            self.velY = math.sin(self.angle) + self.speed
+
+            self.setPosition(self.x + self.velX, self.y + self.velY)
         else:
             pass
-        
+
 class AntManager(Ant):
     def __init__(self, window):
         self.window = window
