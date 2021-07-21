138e1ae26a45e6e34fc3eaace39fab0deea9edc9
Joseph <mateuszpieczulis123@gmail.com>
2021-07-21 15:50:34 +0200

    fixed ant movement

--- modules/ant.py
+++ modules/ant.py
@@ -9,14 +9,12 @@ class Sensor():
         self.x = x
         self.y = y
 
-        self.scale = 4
+        self.scale = 2
         self.antRadius = antRadius
         self.radius = self.antRadius * self.scale
 
         self.offset = self.setOffsetByID(id_)
-
-    def sense(self):
-        pass
+        self.strenght = 0
 
     def testDraw(self, surface):
         rect = pygame.draw.circle(surface, colors['white'], (self.x+self.offset[0], self.y+self.offset[1]), self.radius)
@@ -40,48 +38,89 @@ class Sensor():
         elif id_ == 2:
             return (distance, -distance)
 
+    def detect(self):
+        pass
+
 class Ant(Sensor):
-    def __init__(self, surface, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
 
         self.radius = 2
 
-        self.velX = 0.0
-        self.velY = 0.0
-
         self.angle = 0
         self.rotationSpeed = 15
-        self.speed = 1
+        self.speed = 0.50
+        self.distanceMax = 250
 
-        self.targetPos = (0, 0)
-        self.lastDirection = self.angle
+        self.tslthDefault = 120
+        self.tslth = self.tslthDefault # Time since last target hit
+
+        self.targetPos = self.createTarget()
+        self.steps = self.getSteps()
 
         self.searching = True
         self.sensors = [Sensor(self.x, self.y, 0, self.radius), Sensor(self.x, self.y, 1, self.radius), Sensor(self.x, self.y, 2, self.radius)]
 
     def draw(self, surface):
-        rect = pygame.draw.circle(surface, colors['grey'], (self.x, self.y), self.radius)
+        pygame.draw.circle(surface, colors['grey'], (self.x, self.y), self.radius)
+
         for sensor in self.sensors:
             sensor.testDraw(surface)
 
+        self.testDrawTarget(surface)
+
+    def testDrawTarget(self, surface): #TODO DELETE
+        pygame.draw.circle(surface, colors['yellow'], self.targetPos, 5)
+        pygame.draw.line(surface, colors['yellow'], (self.x, self.y), self.targetPos, 1)
+
     def setPosition(self, x, y):
         self.x, self.y = x, y
+        self.tslth -= 1
         for sensor in self.sensors:
             sensor.setPosition(self.x, self.y)
 
+    def createTarget(self):
+        target = (0, 0)
+        direction = random.randint(0 , 3)
+        distanceX = random.randint(0, self.distanceMax)
+        distanceY = random.randint(0, self.distanceMax)
+
+
+        if direction == 0:
+            target = (self.x + distanceX, self.y + distanceY)
+        elif direction == 1:
+            target = (self.x + distanceX, self.y - distanceY)
+        elif direction == 2:
+            target = (self.x - distanceX, self.y - distanceY)
+        elif direction == 3:
+            target = (self.x - distanceX, self.y + distanceY)
+
+        if target[0] <= 0 or target[1] <= 0:
+            target = self.createTarget()
+        elif target[0] >= 1620 or target[1] >= 1080:
+            target = self.createTarget()
+        return target
+
+    def getSteps(self):
+        dx, dy = (self.targetPos[0] - self.x, self.targetPos[1] - self.y)
+        return (dx / 60., dy / 60.)
+
+    def newTarget(self):
+        self.targetPos = self.createTarget()
+        self.steps = self.getSteps()
+        self.speed = random.random()
+        self.tslth = self.tslthDefault
+
     def move(self):
+        self.searching = self.checkSensors()
+
         if self.searching:
-            self.rotationSpeed = random.randint(0, 20)
-            self.speed = random.randint(0, 2)
-            angleNew = random.randint(0 , 360)
-
-            self.angle = abs(self.angle - angleNew - self.rotationSpeed)
-
-            self.velX = math.cos(self.angle) + self.speed
-            self.velY = math.sin(self.angle) + self.speed
-
-            self.setPosition(self.x + self.velX, self.y + self.velY)
+            if int(self.x) == int(self.targetPos[0]) and int(self.y) == int(self.targetPos[1]):
+                self.newTarget()
+            else:
+                self.setPosition(self.x + self.steps[0] * self.speed, self.y + self.steps[1] * self.speed)
+                if self.tslth <= 0: self.newTarget()
         else:
             pass
 
@@ -91,14 +130,25 @@ class AntManager(Ant):
         self.surface = self.window.surface
         self.antList = []
 
-    def createAnt(self, targetX, targetY):
-        self.antList.append(Ant(self.surface, targetX, targetY))
+    def createAnt(self, x, y):
+        self.antList.append(Ant(x, y))
 
     def draw(self):
         for ant in self.antList:
             ant.draw(self.surface)
 
+    def clearAll(self):
+        self.clearAnts()
+        self.clearTrail()
+
+    def clearAnts(self):
+        self.antList.clear()
+
+    def clearTrail(self):
+        self.trailList.clear()
+
     def moveAnts(self):
         for ant in self.antList:
             ant.move()
+
 
